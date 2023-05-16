e01489007f55d045a733bdae7a785ea89384d952
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-05-16 03:44:28 +0300

    router finished

--- Public/Index.php
+++ Public/Index.php
@@ -1,8 +1,18 @@
-<?php
+<?php declare(strict_types=1);
 
-require_once 'vendor/autoload.php';
+use App\Core\Renderer;
+use App\Core\Router;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
+require_once '../vendor/autoload.php';
 
+$path = Router::route();
 
+$renderer = new Renderer();
 
-
+try {
+    echo $renderer->render($path);
+} catch (LoaderError|SyntaxError|RuntimeError $e) {
+}
