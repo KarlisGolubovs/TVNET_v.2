e1b2c6c8f9c1971575e2a87e530cafce96ba0077
KarlisGolubovs <karlisgolubovs@gmail.com>
2023-05-30 16:11:59 +0300

    database rework

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -1,11 +1,37 @@
 <?php declare(strict_types=1);
 
-namespace App\Core;
-
-use Doctrine\DBAL\{Connection, DriverManager};
-
+use mysql_xdevapi\DatabaseObject;
 
 class Database
 {
+    private string $host;
+    private stdClass $database;
+    private DatabaseObject $username;
+    private DatabaseObject $password;
+    private $connection;
 
+    public function __construct($host, $database, $username, $password)
+    {
+        $this->host = $host;
+        $this->database = $database;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function connect()
+    {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->database}";
+            $this->connection = new PDO($dsn, $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected to the database successfully.";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
 }
